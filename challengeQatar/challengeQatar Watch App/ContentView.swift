5dd577582e2790d0cc42f72dbd575cdedbeb0d2a
joaopbarret <jpbol@cin.ufpe.br>
2022-11-29 16:02:37 -0300

    Adicionado padroes de vibracao

--- challengeQatar/challengeQatar Watch App/ContentView.swift
+++ challengeQatar/challengeQatar Watch App/ContentView.swift
@@ -15,11 +15,65 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
                 .onTapGesture{
-                    WKInterfaceDevice.current().play(.stop)
+                    while 1<2
+                    {
+                        maracaturapido()
+                    }
                 }
         }
         .padding()
     }
+}
+
+func marchadobrado(){
+    WKInterfaceDevice.current().play(.click)
+    usleep(500000)
+    WKInterfaceDevice.current().play(.start)
+    usleep(500000)
+}
+
+func valsa2 (){
+    WKInterfaceDevice.current().play(.click)
+    
+    usleep(517241)
+    
+    WKInterfaceDevice.current().play(.click)
+    
+    usleep(517241)
+    WKInterfaceDevice.current().play(.start)
+    usleep(517241)
+}
+
+func maracatulento(){
+    WKInterfaceDevice.current().play(.click)
+    usleep(500000)
+    usleep(500000)
+    WKInterfaceDevice.current().play(.click)
+    usleep(500000)
+    WKInterfaceDevice.current().play(.click)
+    usleep(500000)
+}
+
+func maracaturapido(){
+    WKInterfaceDevice.current().play(.click)
+    usleep(428571)
+    usleep(428571)
+    WKInterfaceDevice.current().play(.click)
+    usleep(428571)
+    WKInterfaceDevice.current().play(.click)
+    usleep(428571)
+}
+
+func valsa1 (){
+    WKInterfaceDevice.current().play(.click)
+    
+    usleep(461538)
+    
+    WKInterfaceDevice.current().play(.click)
+    
+    usleep(461538)
+    WKInterfaceDevice.current().play(.start)
+    usleep(461538)
 }
 
 struct ContentView_Previews: PreviewProvider {
